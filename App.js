1035eb9e03f9328c8d49ad8813fe88476072d570
MichelBDLC <michelbdlc@gmail.com>
2023-03-12 09:24:06 -0700

    fetch sect

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 //import { useState } from "react";
 
 //data and info passing down to ALL childs here
 
 function App() {
+
+  const [searchData, setSearchData] = useState(null);
 
   return (
     <>
